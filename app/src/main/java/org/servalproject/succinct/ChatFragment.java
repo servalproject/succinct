2a8e84e68d7a784b5c0809877ef083bf5d471a5b
Kieran Clancy <kieran.clancy@flinders.edu.au>
2017-08-04 13:43:52 +0930

    Use AsyncTaskLoader rather than AsyncTask in ChatFragment

--- app/src/main/java/org/servalproject/succinct/ChatFragment.java
+++ app/src/main/java/org/servalproject/succinct/ChatFragment.java
@@ -1,9 +1,13 @@
 package org.servalproject.succinct;
 
 
-import android.os.AsyncTask;
+import android.app.LoaderManager;
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+import android.content.Loader;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,13 +18,10 @@ import org.servalproject.succinct.chat.ChatAdapter;
 import org.servalproject.succinct.chat.ChatDatabase;
 import org.servalproject.succinct.chat.ChatDatabase.ChatMessageCursor;
 
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
 
-
-public class ChatFragment extends Fragment {
+public class ChatFragment extends Fragment implements LoaderManager.LoaderCallbacks<ChatMessageCursor> {
     private static final String TAG = "ChatFragment";
+    private ChatAdapter adapter;
 
     public ChatFragment() {
         // Required empty public constructor
@@ -40,30 +41,84 @@ public class ChatFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_chat, container, false);
         RecyclerView recycler = (RecyclerView) view.findViewById(R.id.chatRecycler);
-        ChatAdapter adapter = new ChatAdapter();
+        adapter = new ChatAdapter();
         recycler.setAdapter(adapter);
         adapter.enableStickyScroll(recycler);
-        new ConnectChatDatabase().execute(adapter);
         return view;
     }
 
-    private class ConnectChatDatabase extends AsyncTask<ChatAdapter, Void, ChatMessageCursor> {
-        private ChatAdapter adapter;
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        Log.d(TAG, "getLoaderManager().initLoader()");
+        getLoaderManager().initLoader(0, null, this);
+    }
+
+    @Override
+    public Loader<ChatMessageCursor> onCreateLoader(int i, Bundle args) {
+        Log.d(TAG, "onCreateLoader");
+        return new ChatDatabaseLoader(getActivity());
+    }
+
+    @Override
+    public void onLoadFinished(Loader<ChatMessageCursor> loader, ChatMessageCursor cursor) {
+        Log.d(TAG, "onLoadFinished");
+        adapter.setCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<ChatMessageCursor> loader) {
+        Log.d(TAG, "onLoaderReset");
+        adapter.setCursor(null);
+    }
+
+    private static class ChatDatabaseLoader extends AsyncTaskLoader<ChatMessageCursor> {
+        ChatMessageCursor cursor;
+
+        private ChatDatabaseLoader(Context context) {
+            super(context);
+        }
+
         @Override
-        protected ChatMessageCursor doInBackground(ChatAdapter... adapters) {
-            adapter = adapters[0];
-            ChatDatabase db = ChatDatabase.getInstance(getActivity().getApplicationContext());
+        public ChatMessageCursor loadInBackground() {
+            Log.d(TAG, "ChatDatabaseLoader loadInBackground");
+            ChatDatabase db = ChatDatabase.getInstance(getContext().getApplicationContext());
             return db.getChatMessageCursor();
         }
 
         @Override
-        protected void onPostExecute(ChatMessageCursor chatMessageCursor) {
-            if (adapter != null) {
-                adapter.setCursor(chatMessageCursor);
-            } else {
-                chatMessageCursor.close();
-                Log.e(TAG, "ConnectChatDatabase called without valid adapter");
+        public void deliverResult(ChatMessageCursor c) {
+            if (isReset()) {
+                c.close();
             }
+            ChatMessageCursor old = cursor;
+            cursor = c;
+
+            if (isStarted()) {
+                super.deliverResult(c);
+            }
+
+            if (old != null && old != c) {
+                old.close();
+            }
+        }
+
+        @Override
+        protected void onStartLoading() {
+            Log.d(TAG, "ChatDatabaseLoader onStartLoading");
+            if (cursor != null) {
+                // already have cursor
+                deliverResult(cursor);
+            }
+            if (takeContentChanged() || cursor == null) {
+                forceLoad();
+            }
+        }
+
+        @Override
+        protected void onStopLoading() {
+            cancelLoad();
         }
     }
 }
